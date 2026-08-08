f4105857765d4b6038e4fc25f6b473a08ca92858
fzdhl <fadhil162006@gmail.com>
2026-08-08 17:35:18 +0700

    fix: set html canvas background to #001207 and theme-color to prevent white overscroll rubberbanding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#001207">
     <title>@yield('title', config('app.name'))</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
